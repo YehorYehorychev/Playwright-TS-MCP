bff79d167717f0e65da0945785eec49a698d672e
Yehor Yehorychev <egor57899@gmail.com>
2025-10-30 15:27:35 -0700

    Login flow test has been added

--- tests/demo/testgen-demo.spec.ts
+++ tests/demo/testgen-demo.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("TestGen demo", async ({ page }) => {
   // Recorded test (TestGen)
   await page.goto("https://katalon-demo-cura.herokuapp.com/");
   await page.getByRole("link", { name: "Make Appointment" }).click();
+
   await page.getByLabel("Username").click();
   await page.getByLabel("Username").fill("John Doe");
   await page.getByLabel("Password").click();
+
   await page.getByLabel("Password").fill("ThisIsNotAPassword");
   await page.getByRole("button", { name: "Login" }).click();
 
