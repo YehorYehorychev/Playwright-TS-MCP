89388545bc505273f238047c716b38dde8851f16
Yehor Yehorychev <egor57899@gmail.com>
2025-11-10 15:01:22 -0800

    Get list of teh users API test has been written

---
+++ tests/api/users.api.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect, request } from "@playwright/test";
+import { log } from "../helpers/logger";
+
+test.describe("REST API Demo", () => {
+  const baseURl = "https://reqres.in/api";
+
+  test("Should GET the list of users", async ({ request }) => {
+    // Make a GET call
+    await log("info", `Making a GET call using ${baseURl}`);
+    const response = await request.get(`${baseURl}/users?page=2`, {
+      headers: {
+        "x-api-key": "reqres-free-v1",
+      },
+    });
+
+    // Assert the status code
+    expect(response.status()).toBe(200);
+    await log("info", `The status code is ${response.status()}`);
+  });
+});
