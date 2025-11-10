56fcbb48dd4160b196aa002e0e35925d9877c2ca
Yehor Yehorychev <egor57899@gmail.com>
2025-11-10 15:22:37 -0800

    POST | Create a new user API test has been written

--- tests/api/users.api.spec.ts
+++ tests/api/users.api.spec.ts
@@ -2,12 +2,12 @@ import { test, expect, request } from "@playwright/test";
 import { log } from "../helpers/logger";
 
 test.describe("REST API Demo", () => {
-  const baseURl = "https://reqres.in/api";
+  const baseURL = "https://reqres.in/api";
 
   test("Should GET the list of users", async ({ request }) => {
     // Make a GET call
-    await log("info", `Making a GET call using ${baseURl}`);
-    const response = await request.get(`${baseURl}/users?page=2`, {
+    await log("info", `Making a GET call using ${baseURL}`);
+    const response = await request.get(`${baseURL}/users?page=2`, {
       headers: {
         "x-api-key": "reqres-free-v1",
       },
@@ -15,6 +15,38 @@ test.describe("REST API Demo", () => {
 
     // Assert the status code
     expect(response.status()).toBe(200);
+    await log("info", `The status code is ${response.status()}`);
+
+    // Get list of the users
+    const responseData = await response.json();
+    await log(
+      "info",
+      `The response is: 
+        ${JSON.stringify(responseData)}`
+    );
+  });
+
+  test("Should Create the user", async ({ request }) => {
+    // Make a POST call
+    await log("info", `Making a POST call using ${baseURL}`);
+
+    const payload = {
+      name: "Yehor Yehorychev",
+      job: "SDET",
+      id: "778",
+      createdAt: "2025-11-10T23:17:13.986Z",
+    };
+
+    const response = await request.post(`${baseURL}/users`, {
+      headers: {
+        "x-api-key": "reqres-free-v1",
+        "Content-Type": "application/json",
+      },
+      data: payload,
+    });
+
+    // Assert the status code
+    expect(response.status()).toBe(201);
     await log("info", `The status code is ${response.status()}`);
 
     // Get list of the users
