2ad69fac54cd600dc970636921fab1fd162100a4
Yehor Yehorychev <egor57899@gmail.com>
2025-11-11 16:14:20 -0800

    NOPCommerce E2E test has been refacored

--- tests/e2e/nopcommerce.e2e.spec.ts
+++ tests/e2e/nopcommerce.e2e.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from "@playwright/test";
 import { log } from "../helpers/logger.js";
 import HomePage from "../page-objects/nopcommerce.home.page.js";
+import CustList from "../page-objects/nopcommerce.custlist.page.js";
 
-test("Login to NopCommerce Web App", async ({ page }, testInfo) => {
-  // Env config
-  const envConfig = testInfo.project.use as any;
+test.describe("E2E Customer Search", () => {
+  test("Login to NopCommerce Web App", async ({ page }, testInfo) => {
+    // Env config
+    const envConfig = testInfo.project.use as any;
 
-  // Create an page object
-  const homePage = new HomePage(page);
+    // Create an page object
+    const homePage = new HomePage(page);
 
-  //Login
-  await homePage.loginToNopeCommerceApp(
-    envConfig.nopCommerceWeb,
-    process.env.NOP_COMMERCE_USER_NAME,
-    process.env.NOP_COMMERCE_PASSWORD
-  );
+    //Login
+    await homePage.loginToNopeCommerceApp(
+      envConfig.nopCommerceWeb,
+      process.env.NOP_COMMERCE_USER_NAME,
+      process.env.NOP_COMMERCE_PASSWORD
+    );
+  });
 });
