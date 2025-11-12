5d47480e4920e2e411f2ba71519fef2bdc289291
Yehor Yehorychev <egor57899@gmail.com>
2025-11-11 16:39:04 -0800

    NOPCommerce E2E test has been refactored

--- tests/e2e/nopcommerce.e2e.spec.ts
+++ tests/e2e/nopcommerce.e2e.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { log } from "../helpers/logger.js";
 import HomePage from "../page-objects/nopcommerce.home.page.js";
 import CustList from "../page-objects/nopcommerce.custlist.page.js";
+import constants from "../../data/constants.json";
 
 test.describe("E2E Customer Search", () => {
   test("E2E_TC001: Search the external customers in customer portal", async ({
@@ -11,37 +12,61 @@ test.describe("E2E Customer Search", () => {
     // Env config
     const envConfig = testInfo.project.use as any;
 
+    //**1 Get list of users */
+    // Make a GET call
+    await log("info", `Making a GET call using ${envConfig.apiURL}`);
+    const res = await request.get(
+      `${envConfig.apiURL}${constants.REQ_RES_ENDPOINTS.GET_USERS_LIST}`,
+      {
+        headers: {
+          "x-api-key": process.env.RES_RES_API_KEY,
+        },
+      }
+    );
+
+    // Assert the status code
+    expect(res.status()).toBe(200);
+    await log("info", `The GET call is succesfull with ${res.status()}`);
+
+    // Get list of users
+    const userData = await res.json();
+    log("info", `List of users: ${JSON.stringify(userData)}`);
+
     // Create an page object
     const homePage = new HomePage(page);
 
-    //Login
+    //**2 Login to WEB */
     await homePage.loginToNopeCommerceApp(
       envConfig.nopCommerceWeb,
       process.env.NOP_COMMERCE_USER_NAME,
       process.env.NOP_COMMERCE_PASSWORD
     );
 
-    // Customer Search
+    /** 3. Customer search */
+    const USER_DATA = userData.data;
     const customerListPage = new CustList(page);
-    customerListPage.goToCustomerListPage(
+    await customerListPage.goToCustomerListPage(
       `${envConfig.nopCommerceWeb}/Admin/Customer/List`
     );
 
-    const MOCK_USER = {
-      firstName: "Yehor",
-      lastName: "Yehorychev",
-    };
-
-    let customerNotFound = await customerListPage.searchAndConfirmUser(
-      MOCK_USER.firstName,
-      MOCK_USER.lastName
-    );
-
-    if (customerNotFound) {
-      await log(
-        "warn",
-        `The given user: ${MOCK_USER.firstName} ${MOCK_USER.lastName} could not be found in the portal. Please try to use a different name.`
+    // Iterate over the list of users
+    for (const user of USER_DATA) {
+      let customerNotFound = await customerListPage.searchAndConfirmUser(
+        user.first_name,
+        user.last_name
       );
+
+      if (customerNotFound) {
+        await log(
+          "warn",
+          `The giver user: ${user.first_name} ${user.last_name} could not found in the portal`
+        );
+      } else {
+        await log(
+          "info",
+          `The giver user: ${user.first_name} ${user.first_name} found in the portal`
+        );
+      }
     }
   });
 });
